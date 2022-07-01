f1825428c34eea05758ea935feb007ebe4e42a1f
Student <student@avans.nl>
2022-07-01 16:04:29 +0200

    20220701

--- install/install.bash
+++ install/install.bash
@@ -4,6 +4,7 @@ cd $HOME/hrwros_v2_ws/src
 sudo apt install net-tools
 
 git clone https://github.com/ros-industrial/universal_robot.git
+git clone -b kinetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 
 echo "export TURTLEBOT3_MODEL=waffle_pi" >> ~/.bashrc
 
@@ -13,8 +14,7 @@ rosdep install --from-paths src --ignore-src -r -y
 
 sudo apt install ros-melodic-fake-localization
 sudo apt-get install ros-melodic-dwa-local-planner
+sudo apt install ros-melodic-teleop-twist-keyboard
 
 
 #sudo adduser $USER dialout
-
-
